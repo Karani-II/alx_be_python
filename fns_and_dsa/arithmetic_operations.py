54c919a6ed86088a600398866ea56a1625917249
Karani <karanik509@gmail.com>
2025-07-27 21:55:21 +0300

    New arithmetic function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation (num1: float, num2: float, operation: str) -> float:
+  if operation == "add":
+    return num1 + num2
+  elif operation == "subtract":
+    return num1 - num2
+  elif operation == "multiply":
+    return num1 * num2
+  elif operation == "divide":
+    if num2 == 0:
+        print("Error: Division by zero is not allowed.")
+    else:
+        return num1 / num2
+  else:
+     print("Error: invalid operation specified please use add, subtract, multiply or divide")
+     return None
