663020bf9b89f16776563c1b2440d499f32bff8d
Karani <karanik509@gmail.com>
2025-08-02 21:25:15 +0300

    Bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,12 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = 500
+
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self.account_balance
+    def withdraw(self, amount):
+        self.account_balance -= amount
+        return True if self.account_balance >= amount else False
+    def display_balance(self, amount):
+        print(f"Hello, you current balance is: {self.account_balance}")
