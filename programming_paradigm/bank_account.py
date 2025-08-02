50a214fd3020b97fad01051ae4d55c274eaaf931
Karani <karanik509@gmail.com>
2025-08-02 22:36:44 +0300

    Bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,14 +1,13 @@
 class BankAccount:
-    def __init__(self, account_balance):
-        self.account_balance = 0
-
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
     def deposit(self, amount):
         self.account_balance += amount
         return self.account_balance
     def withdraw(self, amount):
         if self.account_balance >= amount:
          self.account_balance -= amount
-         print(f"Withdrew: {amount}")
+         print(f"Withdrew: ${amount}")
          return True
         print("Insufficient funds.")
         return False
