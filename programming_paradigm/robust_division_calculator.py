f04a254503e11fd4e22449519e2b0e6919376665
Karani <karanik509@gmail.com>
2025-08-03 13:01:43 +0300

    division calculator

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,9 @@
+def safe_divide(numerator, denominator):
+try:
+    if denominator == 0:
+         raise ZeroDivisionError("Denominator by zero is not allowed.")
+    return numerator / denominator
+except ZeroDivisionError as e:
+     return str(e)
+except ValueError:
+     return "Invalid input: Please enter numeric values."
