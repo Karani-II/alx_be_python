84e4faa192e4da2e6bd0b8edec6293646aa48cd7
Karani <karanik509@gmail.com>
2025-08-03 16:04:01 +0300

    Library management

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -8,13 +8,13 @@ class Library:
  def __init__(self):
         self._books = []
  def add_book(self, book):
-        self.books.append(book)
+        self._books.append(book)
  def check_out_book(self, book):
         if book in self.books:
-            self.books.remove(book)
+            self._books.remove(book)
             book._is_checked_out = True
  def return_book(self):
-        self.books.append(book)
+        self._books.append(book)
         book._is_checked_out = False
  def list_available_books(self):
         return [book for book in self.books if not book._is_checked_out]
