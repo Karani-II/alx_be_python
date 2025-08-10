a6f3097c8f10bae14163507308038eedfc5bc576
Karani <karanik509@gmail.com>
2025-08-10 20:15:18 +0300

    Polymorphism/methos_overiding

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,16 +1,16 @@
 import math
-class shape:
+class Shape:
     def __init__(self, length, width):
         self.length = length
         self.width = width
     def area(self):
         raise NotImplementedError("Subclasses must implement this method")
-class rectangle(shape):
+class Rectangle(Shape):
     def __init__(self, length, width):
         super().__init__(length, width)
     def area(shape):
         return shape.length * shape.width
-class Circle(shape):
+class Circle(Shape):
     def __init__(self, length, width,radius)
         super().__init__(length, width)
         self.radius = radius 
