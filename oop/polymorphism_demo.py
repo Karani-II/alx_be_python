bd97cfb3ff16e260a69b441ce2a6efd833c4115b
Karani <karanik509@gmail.com>
2025-08-10 20:27:06 +0300

    Polymorphism/method_overiding

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -13,10 +13,7 @@ class Rectangle(Shape):
     def area(self):
         return self.length * self.width
 class Circle(Shape):
-    def __init__(self, length, width, radius):
-        super().__init__(length, width)
-        self.length = length
-        self.width = width
+    def __init__(self, radius):
         self.radius = radius 
     def area(self):
         return math.pi * self.radius ** 2
